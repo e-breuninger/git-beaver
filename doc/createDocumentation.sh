d92bb858061b0927f609b47e2e783b45e9c5167a
Jan Kneissler <jan.kneissler@breuninger.de>
2025-05-02 19:57:19 +0200

    dummy change

--- doc/createDocumentation.sh
+++ doc/createDocumentation.sh
@@ -1,3 +1,3 @@
-docker pull gitbeaver/release
+docker pull gitbeaver/release:main
 rm -rf doc/autocreated
-docker run -v $PWD/doc:/workdir gitbeaver/release main=create_docu
+docker run -v $PWD/doc:/workdir gitbeaver/release:main main=create_docu
